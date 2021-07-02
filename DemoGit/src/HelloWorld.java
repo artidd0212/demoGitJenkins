fb173438e799927545992d57261402c074c8feaf
Arti <Arti@LAPTOP-83UQE4T6>
2021-07-02 14:47:49 +0530

    commit2

--- DemoGit/src/HelloWorld.java
+++ DemoGit/src/HelloWorld.java
@@ -7,9 +7,11 @@ public class HelloWorld
 		System.out.println("Hello First Java Program");
 		System.out.println("Hello main method");
 		System.out.println("Hello......World.....!!!!!!");
+		
 	}
         public void m1()
 	{ 
 		System.out.println("method m1");
+		System.out.println("end ");
 	}
 }
